afc47fb264b5430b8bfda551ff1a3218a59dc9c1
Lilia <lillyallenrowland@gmail.com>
2023-09-30 20:00:00 -0400

    Solve: Valid Parentheses

---
+++ solutions/python/valid_parentheses.py
@@ -0,0 +1,9 @@
+def valid_parentheses(s):
+    stack = []
+    pairs = {'(': ')', '[': ']', '{': '}'}
+    for char in s:
+        if char in pairs:
+            stack.append(char)
+        elif not stack or pairs[stack.pop()] != char:
+            return False
+    return len(stack) == 0
