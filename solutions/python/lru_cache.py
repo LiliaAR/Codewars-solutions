d5cd4f6b868315e9b176b725dc6672e21c5f4957
Lilia <lillyallenrowland@gmail.com>
2024-04-15 21:00:00 -0400

    Solve: LRU Cache

---
+++ solutions/python/lru_cache.py
@@ -0,0 +1,12 @@
+from collections import OrderedDict
+
+class LRUCache:
+    def __init__(self, capacity):
+        self.cache = OrderedDict()
+        self.capacity = capacity
+    
+    def get(self, key):
+        if key not in self.cache:
+            return -1
+        self.cache.move_to_end(key)
+        return self.cache[key]
