66908dce15c75453b96d6a1cc87a51aadb0e9f28
Lilia <lillyallenrowland@gmail.com>
2024-10-28 19:00:00 -0400

    Solve: Sliding Window Maximum

---
+++ solutions/python/sliding_window.py
@@ -0,0 +1,13 @@
+def max_sliding_window(nums, k):
+    from collections import deque
+    result = []
+    dq = deque()
+    for i in range(len(nums)):
+        while dq and dq[0] < i - k + 1:
+            dq.popleft()
+        while dq and nums[dq[-1]] < nums[i]:
+            dq.pop()
+        dq.append(i)
+        if i >= k - 1:
+            result.append(nums[dq[0]])
+    return result
