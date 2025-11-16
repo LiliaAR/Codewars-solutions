1a466158ce3531794588d536d980459840327c64
Lilia <lillyallenrowland@gmail.com>
2025-11-15 20:00:00 -0500

    Solve: Permutations (Backtracking)

---
+++ solutions/python/backtracking.py
@@ -0,0 +1,11 @@
+def permutations(nums):
+    result = []
+    def backtrack(path, remaining):
+        if not remaining:
+            result.append(path[:])
+            return
+        for i in range(len(remaining)):
+            backtrack(path + [remaining[i]], remaining[:i] + 
+remaining[i+1:])
+    backtrack([], nums)
+    return result
